8e88d5f60dbdac32fc62e1abb0ce2ccb53314a32
Gian Eric <gianeric08@gmail.com>
2025-11-09 17:46:07 -0300

    :recycle: auth gemini com env local

--- backend/app/configs/auth_gemini.py
+++ backend/app/configs/auth_gemini.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 class AuthGemini:
     def __init__(self):
-        pass
+        if not os.getenv("GEMINI_API_KEY"):
+            load_dotenv()
 
     def authorization(self):
-        load_dotenv()
-        GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-        if not GEMINI_API_KEY:
-            raise ValueError("GEMINI_API_KEY is not set in environment variables.")
-        return GEMINI_API_KEY
+        gemini_key = os.getenv("GEMINI_API_KEY")
+        if not gemini_key:
+            raise ValueError("Variável de ambiente GEMINI_API_KEY não encontrada.")
+        return gemini_key
